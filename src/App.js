66ec68af50a16242a86fba6506fb4fc6ea40f1fe
Muhammad Hassan Qureshi <sarafahub@gmail.com>
2021-10-18 18:48:34 +0500

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import './App.css';
 // import Counter from './components/Counter';
 // import Toggle from './components/Toggle';
-import Tracker from './components/Tracker';
+// import Tracker from './components/Tracker';
+import Tracker1 from './components/Tracker1';
 
 function App() {
   return (
-    <div className = "App App-header">
-     {/* <div className = "App"> */}
+    // <div className = "App App-header">
+     <div className = "App">
 
     {/* <Counter />
     <Toggle /> */}
-    <Tracker />
+    {/* <Tracker /> */}
+    <Tracker1 />
     </div>
 
   );
